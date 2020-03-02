9db08c4b705efa0576e99f037676d63e6279cce7
mcodt1702 <marco@mcnyc.net>
2020-03-02 12:30:36 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -3,24 +3,55 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+              return (
+              <div className="resumeS"><header>
+              <h1>Sally Student Resume</h1>
+              </header>
+            <img src="https://m.media-amazon.com/images/M/MV5BMTY5NzE3NzU3MF5BMl5BanBnXkFtZTgwMjg0NTQ5MDE@._V1_.jpg" alt="A super cute picture of Sally Student"/>
+            <address>501 East 17th Street
+              New York, NY 10010
+              <div>347-685-3333</div>
+              <a href="Mailto:sally@gmail.com">Sally@gmail.com</a>
+                
+                
+                </address>
+
+              
+            <main>Job Experience</main>
+              <section>
+                <div>Software Engineer - 09/2015 to 05/2019
+                Luna Software, New York
+                <ul>
+                  <li>Investigation, design, and implement scalable applications for data identification, analysis, retrieval, and indexing.</li>
+                  <li> design and development while remaining concentrate on client needs.</li>
+                  <li> diligently with other IT team members to plan, design, and develop smart solutions.</li>
+                </ul>
+                </div>
+
+
+                <div>Junior Software Engineer - 09/2014 to 09/2015
+                AdsPro Software, New York
+                  <ul>
+                    <li> regularly with customers on project status, proposals and technical issues.</li>
+                    <li>Transformed existing software to correct errors, upgrade interfaces, and improve efficiency.</li>
+                    <li>Cooperate diligently with other IT team members to plan, design, and develop smart solutions.</li>
+                  </ul>
+                </div>        
+                </section>
+                 
+
+                  <div>Software Engineering - 2014
+            Columbia University, NY</div>
+            </div>
+            )
+
+};
+
+
+
+
+
+    
+
 
 export default App;
